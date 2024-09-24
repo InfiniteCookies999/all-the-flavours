bc16245528d7300756041c217bbd45117473c45a
InfiniteCookies999 <infinitecookies959@gmail.com>
2024-09-24 16:52:26 -0400

    Setup basic security filter to allow access to pages

--- server/src/main/java/com/infinitecookies959/gmail/com/all_the_flavours/SecurityConfig.java
+++ server/src/main/java/com/infinitecookies959/gmail/com/all_the_flavours/SecurityConfig.java
@@ -2,14 +2,32 @@ package com.infinitecookies959.gmail.com.all_the_flavours;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                // Disable Cross-Origin Request protection because we want the frontend
+                // to still be able to make requests.
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        // Default to permitting all requests.
+                        .anyRequest().permitAll()
+                )
+                .build();
     }
 }

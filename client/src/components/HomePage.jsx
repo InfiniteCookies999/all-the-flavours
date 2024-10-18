23155fe0f7b5479c10e091667911456a34940f00
InfiniteCookies999 <infinitecookies959@gmail.com>
2024-10-18 15:03:19 -0400

    Improve home page

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,18 +1,31 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { useError } from "../contexts/ErrorContext";
 import RecipeListItem from "./recipe/RecipeListItem";
 import useResponsiveValue from "../hooks/useResponsitveValue";
+import PrimaryButton from "./PrimaryButton";
+import { AuthContext } from "../contexts/AuthContext";
+
+const LargeButton = ({ children, location, style }) => {
+  return (
+    <PrimaryButton style={{
+      marginTop: '1rem',
+      fontSize: '1.5rem',
+      ...style
+    }}
+    onClick={() => {
+      window.location.href = location;
+    }}>
+      {children}
+    </PrimaryButton>
+  );
+};
 
 const RecipeShowcase = ({ title, recipes, colClass, style }) => {
   return (
-    <div style={{
-      background: '#ddd',
-      padding: '1rem',
-      borderRadius: '10px',
-      ...style
-    }}>
+    <div style={style}>
       <h2>{title}</h2>
+      <hr/>
       <div className="row g-3 mt-2">
         {recipes.map((recipe, index) => (
           <RecipeListItem
@@ -23,6 +36,9 @@ const RecipeShowcase = ({ title, recipes, colClass, style }) => {
             colClass={colClass} />
         ))}
       </div>
+      <LargeButton location={"/recipes"}>
+        Find More Recipes!
+      </LargeButton>
     </div>
   );
 };
@@ -33,6 +49,8 @@ const HomePage = () => {
   const [latestRecipes, setLatestRecipes] = useState(null);
 
   const { setError } = useError();
+
+  const { isLoggedIn } = useContext(AuthContext);
 
   document.title = "Home";
 
@@ -62,7 +80,7 @@ const HomePage = () => {
   }
 
   return (
-    <>
+    <div style={{ padding: '1rem' }}>
       <RecipeShowcase
         title={"Popular recipes!"}
         recipes={popularRecipes}
@@ -74,7 +92,37 @@ const HomePage = () => {
         recipes={latestRecipes}
         colClass={colClass}
         />
-    </>
+      
+      <div style={{
+        marginTop: '3rem'
+      }}>
+        <h3>Want to contribute?</h3>
+        <hr/>
+        {isLoggedIn ? (
+          <>
+            <span style={{ fontSize: '1.2rem', color: 'gray' }}>Create your own recipe and so it can be shared with others!</span>
+            <br/>
+            <LargeButton location={"/create-recipe"}>
+              Create Recipe!
+            </LargeButton>
+          </>
+        ) : (
+          <>
+            <span style={{ fontSize: '1.2rem', color: 'gray' }}>You can add your own recipe if you login or sign up!</span>
+            <br/>
+            <LargeButton location={"/login"}>
+              Login
+            </LargeButton>
+            <LargeButton location={"/sign-up"} style={{
+              marginLeft: '1rem'
+            }}>
+              Sign up
+            </LargeButton>
+          </>
+        )}
+      </div>
+    
+    </div>
   );
 };
 
